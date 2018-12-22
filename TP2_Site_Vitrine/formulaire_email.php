54cdc14e87dd25fd6c400cec69852adc27c663c5
KirihimeNatsuki <enzo.lemarchand@ynov.com>
2018-12-22 17:29:43 +0100

    Ajout des premières pages du tp2

---
+++ TP2_Site_Vitrine/formulaire_email.php
@@ -0,0 +1,50 @@
+<div>
+   <form action="index.php" method="post">
+ <div>
+   <label for="nom">Nom :</label>
+   <input type="text" id="nom" name="nom" />
+ </div>
+ <div>
+   <label for="prenom">Prénom :</label>
+   <input type="text" id="prenom" name="prenom" />
+ </div>
+ <div>
+   <label for="formation">Formation :</label>
+   <input type="text" id="formation" name="formation" />
+ </div>
+ <div>
+     <label for="subject">Sujet :</label>
+     <input type ="text" id="subject" name="subject" />
+ </div>
+ <div>
+   <label for="mail">E-mail :</label>
+   <input type="email" id="mail" name="mail" />
+ </div>
+ <div>
+   <label for="message">Message:</label>
+   <textarea id="message" name="message"></textarea>
+ </div>
+ <div class="button">
+       <input type="submit" name="valider" value="envoyer" />
+   </div>
+</div>
+   <?php
+
+   ini_set( 'display_errors', 1 );
+
+   error_reporting( E_ALL );
+
+   if(isset($_POST['valider'])){
+           $from = "test@mesozogeek.000webhostapp.com";
+           $to = "maurin.margail@ynov.com";
+           $mail = $_POST['mail'];
+           $subject = $_POST['subject'];
+           $message = $_POST['message'];
+           $nom = $_POST['nom'];
+           $prenom = $_POST['prenom'];
+           $formation = $_POST['formation'];
+           $headers = "From " . $nom . " " . $prenom . " en " . $formation . "  " . $mail;
+           mail($to,$subject,$message, $headers);
+           echo "L'email a été envoyé.";
+       }
+?>
